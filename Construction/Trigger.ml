5aa98224b9ca3ca2be6b83406cb54c9330b08f5e
Arthur Milchior <Arthur@Milchior.fr>
2020-05-03 13:12:50 +0200

    local C-function: triggeredBySomeParent

--- Construction/Trigger.ml
+++ Construction/Trigger.ml
@@ -1,3 +1,10 @@
+let triggerredBySomeParent parents fmt ((_, index) as formula) =
+  format_concat ~left:"function triggered_by_some_parent()@ @[{return@ @[("
+    ~right:"@])@;//Whether some parent triggerred this formula@]}"
+    ~sep:"@ &&@ "
+    (fun fmt parent_index -> fprintf fmt "trigger_%d_%d" parent_index index) fmt (Parent.getParentsIndex parents index);;
+;;
+
 let immediatlyTrue fmt formula =
   fprintf fmt "function immediately_true()@ @[{return %a;@;//Whether the MITL holds simply du to immediate properties.@]}" Atemporal.format
 ;;
--- Construction/Trigger.mli
+++ Construction/Trigger.mli
@@ -1,3 +1,9 @@
+(** A function declaration returning whether a parent triggered this
+   subformula. *)
+val triggerredBySomeParent :
+  Parent.t -> Indexed.index -> MlType.declaration
+;;
+
 (** A function declaration returning whether the input atemporal formula is immedietely true. *)
 val immediatlyTrue :
   Format.formatter -> Atemporal.t -> unit
